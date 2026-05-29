fba97e6084d10e414d0d2eee24079ba91344bbc0
Deepak <chdeepak623@gmail.com>
2026-05-29 10:26:45 +0530

    added automated tesing for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,18 @@
+import { formatCurrency } from "../scripts/utils/money.js"; 
+
+console.log("test suite:formatCurrency");
+console.log("works on price in cents");
+if(formatCurrency(2005)==='20.05')
+    console.log("passed");
+else
+    console.log("failed");
+console.log("works with 0");
+if(formatCurrency(0)==='0.00')
+    console.log("passed");
+else
+    console.log("failed");
+console.log("works with rounding cents ")
+if(formatCurrency(2000.5)==='20.01')
+    console.log("passed");
+else
+    console.log("failed");
